df0a2f315d35f558b529097c54ef81d95fb3f7d4
raceface2nd <raceface_the_one@gmx.net>
2016-12-05 22:02:16 +0100

    style update

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,11 +1,19 @@
 <script id="content-tpl" type="text/x-handlebars-template">
     {{#if event}}
+    <p>
         <input type="text" name="start" id="start">
         <input type="text" name="end" id="end">
         <select id="client" name="client">
-            <option value="0" selected=""</option>
+            <option value="0" selected>Select Client</option>
             <option value="1">Client 1</option>
             <option value="2">Client 2</option>
+        </select>
+        <select id="project" name="project">
+            <option value="0" selected>Select Project</option>
+            <option value="1">Project 1</option>
+            <option value="2">Project 2</option>
+        </select>
+        </p>
         <div class="input"><textarea>{{ event.notes }}</textarea></div>
         <div class="save"><button><?php p($l->t('Save')); ?></button></div>
     {{else}}
