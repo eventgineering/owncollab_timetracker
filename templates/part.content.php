ff4323694bf258ceffae51e9c11045fafe89b103
raceface2nd <raceface_the_one@gmx.net>
2016-12-05 21:45:09 +0100

    improve

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,14 +1,12 @@
 <script id="content-tpl" type="text/x-handlebars-template">
     {{#if event}}
-        kjdfkgjnkfjgk
-        <div class="input"><textarea>{{ event.notes }}</textarea></div>
-        <div class="save"><button><?php p($l->t('Save')); ?></button></div>
-    {{else}}
-        	<div class="expirationDateContainer {{#unless isExpirationSet}}hidden{{/unless}}">
+        	<div class="expirationDateContainer">
 			<label for="expirationDate" class="hidden-visually" value="{{expirationDate}}">{{expirationLabel}}</label>
 			<input id="expirationDate" class="datepicker" type="text" placeholder="{{expirationDatePlaceholder}}" value="{{expirationValue}}" />
 			</div>
-
+        <div class="input"><textarea>{{ event.notes }}</textarea></div>
+        <div class="save"><button><?php p($l->t('Save')); ?></button></div>
+    {{else}}
         <div class="input"><textarea disabled></textarea></div>
         <div class="save"><button disabled><?php p($l->t('Save')); ?></button></div>
     {{/if}}
