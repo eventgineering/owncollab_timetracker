ff4e3bff58145547fbf3bad1a6deb57f89950913
raceface2nd <raceface_the_one@gmx.net>
2016-12-05 21:59:30 +0100

    test

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,6 +1,11 @@
 <script id="content-tpl" type="text/x-handlebars-template">
     {{#if event}}
-        <div class="input" type="text">start</div>
+        <input type="text" name="start" id="start">
+        <input type="text" name="end" id="end">
+        <select id="client" name="client">
+            <option value="0" selected=""</option>
+            <option value="1">Client 1</option>
+            <option value="2">Client 2</option>
         <div class="input"><textarea>{{ event.notes }}</textarea></div>
         <div class="save"><button><?php p($l->t('Save')); ?></button></div>
     {{else}}
