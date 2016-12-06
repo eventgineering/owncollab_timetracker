11ede5a3f12ce2f2eedb50d081113c3f1ad05f35
raceface2nd <raceface_the_one@gmx.net>
2016-12-06 14:21:15 +0100

    fix

--- lib/controller/pagecontroller.php
+++ lib/controller/pagecontroller.php
@@ -3,12 +3,16 @@
 
  use OCP\IRequest;
  use OCP\AppFramework\Http\TemplateResponse;
+ use OCP\AppFramework\Http\DataResponse;
  use OCP\AppFramework\Controller;
 
  class PageController extends Controller {
 
-     public function __construct($AppName, IRequest $request){
+     private $userId;
+
+     public function __construct($AppName, IRequest $request, $userId){
          parent::__construct($AppName, $request);
+         $this->userId = $UserId;
      }
 
      /**
@@ -18,5 +22,13 @@
      public function index() {
          return new TemplateResponse('owncollab_timetracker', 'main');
      }
+
+        /**
+         * Simply method that posts back the payload of the request
+         * @NoAdminRequired
+         */
+        public function doEcho($echo) {
+                return new DataResponse(['echo' => $echo]);
+        }
 
  }
