67bde8ec3eb46c7d2e1f55236af27830e0c23127
raceface2nd <raceface_the_one@gmx.net>
2016-12-06 17:48:50 +0100

    test

--- templates/main.php
+++ templates/main.php
@@ -12,17 +12,6 @@ style('ownnotes', 'style');
 
 	<div id="app-content">
 		<div id="app-content-wrapper">
-		    <div class="tbl">
-
-        <div class="tbl_cell width25 valign_top">
-            <?php print_unescaped($this->inc('part.content.list')); ?>
-        </div>
-
-        <div class="loader_min" style="display: none"></div>
-        <div id="r_messages" class="tbl_cell valign_top" style="display: none"></div>
-
-    </div>
-			
 			<?php print_unescaped($this->inc('part.content')); ?>
 		</div>
 	</div>
