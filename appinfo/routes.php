266cae5ac3b834b933b24db219c72aca9c9e9b00
raceface2nd <raceface_the_one@gmx.net>
2016-12-06 17:59:53 +0100

    Revert "Fix"
    This reverts commit 99d4186b8101ce6a14c04df4723829cf999c5d07.

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -19,7 +19,7 @@
  */
 return [
     'resources' => [
-        'note' => ['url' => '/owncollab_timetracker']
+        'note' => ['url' => '/oc_timetracker']
     ],
     'routes' => [
 	   ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
