5ca99a417c60dad010bf43976c9400aa0420f137
raceface2nd <raceface_the_one@gmx.net>
2016-12-06 20:25:43 +0100

    test

--- js/script.js
+++ js/script.js
@@ -236,7 +236,7 @@ View.prototype = {
         $('#app-navigation .event > a').click(function () {
             var id = parseInt($(this).parent().data('id'), 10);
             self._events.load(id);
-            if (event.start!=''){$('#start').val(event.start);}
+            if (self._events.start != ''){$('#start').val(self._events.start);}
             self.render();
             $('#editor textarea').focus();
 
