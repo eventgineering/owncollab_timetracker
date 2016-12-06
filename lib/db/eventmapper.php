6a2c1373b6113a79d4cff448023d0a184558f190
raceface2nd <raceface_the_one@gmx.net>
2016-12-06 18:42:19 +0100

    Finished rework

--- lib/db/eventmapper.php
+++ lib/db/eventmapper.php
@@ -7,16 +7,16 @@ use OCP\AppFramework\Db\Mapper;
 class EventMapper extends Mapper {
 
     public function __construct(IDb $db) {
-        parent::__construct($db, 'ownnotes_notes', '\OCA\OwnCollab_TimeTracker\Db\Event');
+        parent::__construct($db, 'owncollab_timetracker_events', '\OCA\OwnCollab_TimeTracker\Db\Event');
     }
 
     public function find($id, $userId) {
-        $sql = 'SELECT * FROM *PREFIX*ownnotes_notes WHERE id = ? AND user_id = ?';
+        $sql = 'SELECT * FROM *PREFIX*owncollab_timetracker_events WHERE id = ? AND user_id = ?';
         return $this->findEntity($sql, [$id, $userId]);
     }
 
     public function findAll($userId) {
-        $sql = 'SELECT * FROM *PREFIX*ownnotes_notes WHERE user_id = ?';
+        $sql = 'SELECT * FROM *PREFIX*owncollab_timetracker_events WHERE user_id = ?';
         return $this->findEntities($sql, [$userId]);
     }
 
