05bc0528808b3a02dc38bc1f4299aa6c420a16a3
raceface2nd <raceface_the_one@gmx.net>
2016-12-08 12:46:10 +0100

    changed template content

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,7 +1,3 @@
-<p>Hello World <?php p($_['user']) ?></p>
-
-<p><button id="hello">click me</button></p>
-
 <script id="content-tpl" type="text/x-handlebars-template">
     {{#if event}}
 	<p>
@@ -11,6 +7,9 @@
         <div class="input"><textarea placeholder="{{ event.content }}"></textarea></div>
         <div class="save"><button><?php p($l->t('Save')); ?></button></div>
     {{else}}
+    <p>Hello World <?php p($_['user']) ?></p>
+
+    <p><button id="hello">click me</button></p>
 	<p>
         <input type="text" name="start" id="start" placeholder="<?php p($l->t('start date')); ?>" disabled>
         <input type="text" name="end" id="end" placeholder="<?php p($l->t('end date')); ?>" disabled>
