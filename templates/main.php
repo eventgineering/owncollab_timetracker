716206923977ecb5c0d0cc4dec94c6fd053cb6a0
raceface2nd <raceface_the_one@gmx.net>
2016-12-08 13:13:16 +0100

    changed order of scripts

--- templates/main.php
+++ templates/main.php
@@ -1,11 +1,11 @@
 <?php
 script('owncollab_timetracker', 'handlebars');
-script('owncollab_timetracker', 'lib/angular-ui-calendar/src/calendar');
 script('owncollab_timetracker', 'lib/angular/angular');
 script('owncollab_timetracker', 'lib/angular-route/angular-route');
 script('owncollab_timetracker', 'lib/angular-bootstrap/ui-bootstrap.min');
 script('owncollab_timetracker', 'lib/angular-bootstrap/ui-bootstrap-tpls.min');
 script('owncollab_timetracker', 'lib/jquery-timepicker/jquery.ui.timepicker');
+script('owncollab_timetracker', 'lib/angular-ui-calendar/src/calendar');
 script('owncollab_timetracker', 'script');
 style('owncollab_timetracker', 'style');
 style('owncollab_timetracker', 'datepicker');
