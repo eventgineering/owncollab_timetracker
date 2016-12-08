f4c77f1d558ee832e46a740d8ab4d228e5b98bb7
eventgineering <info@eventgineering.de>
2016-12-08 16:19:36 +0100

    clean

--- templates/main.php
+++ templates/main.php
@@ -1,7 +1,5 @@
 <?php
 script('owncollab_timetracker', 'handlebars');
-script('owncollab_timetracker', 'lib/jquery/jquery')
-script('owncollab_timetracker', 'lib/jquery/jquery-ui.js')
 script('owncollab_timetracker', 'script');
 style('owncollab_timetracker', 'style');
 ?>
