9e09595c71a68273e9a3f13f86c6e9517262188b
raceface2nd <raceface_the_one@gmx.net>
2016-12-10 12:37:54 +0100

    added script to main.php

--- templates/main.php
+++ templates/main.php
@@ -6,6 +6,7 @@ script('owncollab_timetracker', 'jquery.localisation');
 script('owncollab_timetracker', $langurl);
 script('owncollab_timetracker', 'timepicki/timepicki');
 script('owncollab_timetracker', 'script');
+script('owncollab_timetracker', 'handle.clients');
 style('owncollab_timetracker', 'timepicki/timepicki');
 style('owncollab_timetracker', 'style');
 ?>
