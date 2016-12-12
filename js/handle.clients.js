43028df0e08c03d231c0ac192f346505c3404c58
raceface2nd <raceface_the_one@gmx.net>
2016-12-12 20:39:01 +0100

    test

--- js/handle.clients.js
+++ js/handle.clients.js
@@ -11,61 +11,17 @@
 (function (OC, window, $, undefined) {
 'use strict';
 
-$(document).ready(function () {
+    $(document).ready(function () {
 
-// this clients object holds all our clients
-var Clients = function (baseUrl) {
-    this._baseUrl = baseUrl;
-    this._clients = [];
-    this._activeClient = undefined;
-};
+        $.getJSON("/index.php/apps/owncollab_timetracker/clients", function(result){
+	        var items="";
+		    $.each(result, function(i, data){
+                items+="<option value='"+data.id+"'>"+data.name+"</option>";
+                console.log('id: ', data.id, 'name: ', data.name);
+	    	});
+            $("#a1_title").append(items); 
+    	});
 
-Clients.prototype = {
-    getAll: function () {
-        return this._clients;
-    },
-    loadAll: function () {
-        var deferred = $.Deferred();
-        var self = this;
-        $.get(this._baseUrl).done(function (clients) {
-            self._activeClient = undefined;
-            self._clients = clients;
-            deferred.resolve();
-        }).fail(function () {
-            deferred.reject();
-        });
-        return deferred.promise();
-    }
-};
-
-// this will be the view that is used to update the html
-var View = function (clients) {
-    this._clients = clients;
-};
-
-View.prototype = {
-    renderClients: function () {
-        var source = $('#content-tpl').html();
-        var template = Handlebars.compile(source);
-        var html = template({clients: this._clients.getAll()});
-
-        $('#editor').html(html);
-        var entry = $(this).closest('.client');
-    },
-    render: function () {
-        this.renderClients();
-    }
-};
-
-var clients = new Clients(OC.generateUrl('/apps/ownclients/clients'));
-var view = new View(clients);
-clients.loadAll().done(function () {
-    view.render();
-}).fail(function () {
-    alert('Could not load clients');
-});
-
-
-});
+    });
 
 })(OC, window, jQuery);
--- js/script.js
+++ js/script.js
@@ -12,15 +12,6 @@
 'use strict';
 
 $(document).ready(function () {
-        $.getJSON("/index.php/apps/owncollab_timetracker/clients", function(result){
-	        var items="";
-		$.each(result, function(i, data){
-                items+="<option value='"+data.id+"'>"+data.name+"</option>";
-                console.log('id: ', data.id, 'name: ', data.name);
-	    	});
-            $("#a1_title").append(items); 
-    	});
-
 
 var translations = {
     newEvent: $('#new-event-string').text()
