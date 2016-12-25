7e28eeb885cad527d2ef3a14c7a8341876ae3643
raceface2nd <raceface_the_one@gmx.net>
2016-12-25 20:16:54 +0100

    style test

--- templates/part.content.projects.php
+++ templates/part.content.projects.php
@@ -6,13 +6,13 @@
     {{#each projects}}
         <li class="project with-menu {{#if active}}active{{/if}}" data-id="{{ id }}">
             <a href="#">{{ name }}</a>
-            <div class="app-navigation-entry-utils">
+            <div class="sub-navigation-entry-utils">
                 <ul>
-                    <li class="app-navigation-entry-utils-menu-button svg"><button></button></li>
+                    <li class="sub-navigation-entry-utils-menu-button svg"><button></button></li>
                 </ul>
             </div>
 
-            <div class="app-navigation-entry-menu">
+            <div class="sub-navigation-entry-menu">
                 <ul>
                     <li><button class="delete icon-delete svg" title="delete"></button></li>
                 </ul>
