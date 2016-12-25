23a47c889476bd246814c1f36ace97f178dff22c
raceface2nd <raceface_the_one@gmx.net>
2016-12-25 18:32:16 +0100

    try

--- js/handle.projects.js
+++ js/handle.projects.js
@@ -176,7 +176,7 @@ View.prototype = {
         // show app menu
         $('#project-sub-navigation .sub-navigation-entry-utils-menu-button').click(function () {
             var entry = $(this).closest('.project');
-            entry.find('.app-navigation-entry-menu').toggleClass('open');
+            entry.find('.sub-navigation-entry-menu').toggleClass('open');
         });
 
         // delete a project
--- templates/part.content.projects.php
+++ templates/part.content.projects.php
@@ -4,18 +4,17 @@
 <script id="projects-tpl" type="text/x-handlebars-template">
     <li id="new-project"><a href="#"><?php p($l->t('Add project')); ?></a></li>
     {{#each projects}}
-        <li class="project with-menu {{#if active}}active{{/if}}" {{#if active}}active{{/if}} data-id="{{ id }}">
-            <input class="sub-navigation-entry-edit" type="text" name="{{ name }}" value="{{ name }}" data-id="{{ id }}"/><a href="#" name="{{ name }}" data-id="{{ id }}">{{ name }}</a>
+        <li class="project with-menu {{#if active}}active{{/if}}" data-id="{{ id }}">
+            <a href="#">{{ name }}</a>
             <div class="sub-navigation-entry-utils">
                 <ul>
                     <li class="sub-navigation-entry-utils-menu-button svg"><button></button></li>
                 </ul>
             </div>
 
-            <div class="sub-navigation-entry-menu bubble menu">
+            <div class="sub-navigation-entry-menu">
                 <ul>
-                    <li data-id="{{ id }}"><a class="menuitem action permanent delete" href="#"><button class="icon-delete svg" title="delete"></button><?php p($l->t('Delete')); ?></a></li><br />
-                    <li data-id="{{ id }}"><a class="menuitem action permanent rename" href="#"><button class="icon-rename svg" title="rename"></button><?php p($l->t('Rename')); ?></a></li>
+                    <li><button class="delete icon-delete svg" title="delete"></button></li>
                 </ul>
             </div>
         </li>
